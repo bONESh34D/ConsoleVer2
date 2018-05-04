fac8459f979f7f4b4f91506b344d6d6b4d7609df
Peter T. LaComb Jr <peter@lacombfamily.com>
2018-05-04 07:25:06 -0400

    OK - A couple things here. 1: Consider breaking each logical group of action out into a function. That's what I started on with the refreshEnvironment method. 2: To make calling that function simpler, I created a small class called RefreshRequest. It's nice when functions take few parameters, and later on you might learn there is a really cool reason for having functions always take one parameter. Seems weird, but bear with me. 3: There is a bit of noise in this change because my editor reformatted the file.

--- Program.cs
+++ Program.cs
@@ -1,20 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data;
-using System.Data.SqlClient;
 
-namespace ConsoleVer2_GitHub
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace ConsoleVer2_GitHub {
+    class Program {
+        static void Main(string[] args) {
             /*Connection*/
-            using (SqlConnection conn = new SqlConnection())
-            {
+            using(SqlConnection conn = new SqlConnection()) {
                 conn.ConnectionString = "Server=PROD\\INSTANCE;Initial Catalog=DATABASE;Integrated Security=True";
                 conn.Open();
 
@@ -40,36 +36,29 @@ namespace ConsoleVer2_GitHub
                 Console.WriteLine("                                                                        ");
                 Console.WriteLine("                                                                        ");
                 Console.WriteLine("__________________________________________________________________________________");
-                Console.WriteLine($"\r\n   Welcome! Thank you for using the APP Lower Environment Refresh Tool (ver 2)   ");
+                Console.WriteLine($"\r\n   Welcome! Thank you for using the APP Lower Environment Refresh Tool(ver 2)   ");
                 Console.WriteLine("__________________________________________________________________________________");
                 Console.WriteLine($"\r\n\r\nPLEASE NOTE: The source of your database will be from " + Source + " Production.");
-                
 
                 /*_Command To Get List_*/
 
                 Console.WriteLine($"\r\n\r\nHere is a list of CompanyCodes and their cooresponding database names for you to choose from:");
                 Console.WriteLine($"_________________________________________________");
 
-                
-                using (SqlCommand selectCommand = new SqlCommand("SELECT [CompanyCode],[CompanyDatabase] FROM [UtilityDatabase].[dbo].[view]", conn))
-                {
+                using(SqlCommand selectCommand = new SqlCommand("SELECT [CompanyCode],[CompanyDatabase] FROM [UtilityDatabase].[dbo].[view]", conn)) {
 
-                    using (SqlDataReader reader = selectCommand.ExecuteReader())
-                    {
+                    using(SqlDataReader reader = selectCommand.ExecuteReader()) {
                         Console.WriteLine("CompanyCode\t|CompanyDatabase\t");
-                        while (reader.Read())
-                        {
+                        while(reader.Read()) {
                             Console.WriteLine(String.Format("    {0} \t        | {1}",
                                 reader[0], reader[1]));
                         }
                     }
                 }
 
-
-                
                 //example of CompanyCode is A1
                 Console.WriteLine($"_________________________________________________");
-                Console.WriteLine($"\r\n\r\nWhich CompanyCode(database shortname) would you like to refresh? Please enter CompanyCode. (ie. A1) :");
+                Console.WriteLine($"\r\n\r\nWhich CompanyCode(database shortname) would you like to refresh? Please enter CompanyCode.(ie. A1) :");
                 CompanyCode = Console.ReadLine();
                 Console.WriteLine($"\r\n\r\nCONFIRMATION: You have chosen the " + CompanyCode + " database. - Please press enter to confirm");
 
@@ -102,7 +91,7 @@ namespace ConsoleVer2_GitHub
                 //example of EmailRequestor is Joe.Blow@domain.com
                 EmailRequestor = ADRequestor + "@domain.com";
                 Console.WriteLine($"\r\n\r\nPlease verify that your email address is " + EmailRequestor + ".");
-                
+
                 Console.ReadKey();
 
                 Console.WriteLine($"\r\n\r\nCONFIRMATION: Your email address is " + EmailRequestor + ".  - Please press enter to confirm");
@@ -126,82 +115,17 @@ namespace ConsoleVer2_GitHub
                 /*_BEGIN_#######_ERROR HANDLING_*/
                 Console.WriteLine($"_________________________________________________");
 
-                Console.WriteLine("Submitting the parameters to the SP. Look for errors beyond this point!");
-
-                /*_try block_*/
-                try
-                {
-                    ///*_#########_BEGIN_ORIG TRY_##########################################################################################################*/
-                    ///*_Create the command to execute!_*/
-                    //                            /*_Test commandfail*/
-                    //                            //SqlCommand commandfail = new SqlCommand("SELECT [FAIL] FROM [msst_test].[dbo].[Fail_vw_FAIL]", conn);
-                    //SqlCommand storedprocCommand = new SqlCommand("Exec [UtilityDatabase].[dbo].[StoredProcedure] @Source = '" + Source + "', @Destination = '" + Destination + "', @Database = '" + Database + "', @Process = '" + Process + "', @CompanyCode = '" + CompanyCode + "', @EmailRequestor = '" + EmailRequestor + "',@ADRequestor = '" + ADRequestor + "'", conn);
-
-                    ///*_Add the parameters._*/
-                    //storedprocCommand.Parameters.Add(new SqlParameter("Source", Source));
-                    //storedprocCommand.Parameters.Add(new SqlParameter("Destination", Destination));
-                    //storedprocCommand.Parameters.Add(new SqlParameter("Database", Database));
-                    //storedprocCommand.Parameters.Add(new SqlParameter("Process", Process));
-                    //storedprocCommand.Parameters.Add(new SqlParameter("CompanyCode", CompanyCode));
-                    //storedprocCommand.Parameters.Add(new SqlParameter("EmailRequestor", EmailRequestor));
-                    //storedprocCommand.Parameters.Add(new SqlParameter("ADRequestor", ADRequestor));
-
-
-                    ////Test commandfail will give on SQL error message
-                    //// - Msg 208, Level 16, State 1, Line 4
-                    ////Invalid object name 'someErrorColumn'.
-                    ///*_ Errors will pop here:_*/
-                    ///*_Note: since we are catching any code block errors, they will display here in the console.*/
-                    //storedprocCommand.ExecuteNonQuery();
-
-                    ///*Try to print the command being sent, make sure to remark out "storedprocCommand.ExecuteNonQuery();"*/
-                    ////Console.WriteLine("___________________________________________________________________________________");
-                    ////Console.WriteLine("Print the command: " + storedprocCommand + ""); //This writes "Print the command: System.Data.SqlClient.SqlCommand"
-                    ////Console.WriteLine("___________________________________________________________________________________");
-                    ///*_#########_END_ORIG TRY_##########################################################################################################*/
-
-                    ///*_#########_BEGIN_NEW TRY_##########################################################################################################*
-                    using (SqlCommand storedprocCommand = new SqlCommand("[dbo].[StoredProcedure]", conn))
-                    {
-                        storedprocCommand.CommandType = CommandType.StoredProcedure;
-                        /*_NOTE:_*/
-                        //cmd.Parameters.Add("@FirstName", SqlDbType.VarChar).Value = txtFirstName.Text;
-                        //cmd.Parameters.Add("@LastName", SqlDbType.VarChar).Value = txtLastName.Text;
-                        /*My try
-                        storedprocCommand.Parameters.Add("@Source", SqlDbType.VarChar).Value = Source;
-                        storedprocCommand.Parameters.Add("@Destination", SqlDbType.VarChar).Value = Destination;
-                        storedprocCommand.Parameters.Add("@Database", SqlDbType.VarChar).Value = Database;
-                        storedprocCommand.Parameters.Add("@Process", SqlDbType.VarChar).Value = Process;
-                        storedprocCommand.Parameters.Add("@CompanyCode", SqlDbType.VarChar).Value = CompanyCode;
-                        storedprocCommand.Parameters.Add("@EmailRequestor", SqlDbType.VarChar).Value = EmailRequestor;
-                        storedprocCommand.Parameters.Add("@ADRequestor", SqlDbType.VarChar).Value = ADRequestor;
-                        */
-                        /*_Note:_Still getting time outs with this_
-                        storedprocCommand.Parameters.Add(new SqlParameter("@Source", Source));
-                        storedprocCommand.Parameters.Add(new SqlParameter("@Destination", Destination));
-                        storedprocCommand.Parameters.Add(new SqlParameter("@Database", Database));
-                        storedprocCommand.Parameters.Add(new SqlParameter("@Process", Process));
-                        storedprocCommand.Parameters.Add(new SqlParameter("@CompanyCode", CompanyCode));
-                        storedprocCommand.Parameters.Add(new SqlParameter("@EmailRequestor", EmailRequestor));
-                        storedprocCommand.Parameters.Add(new SqlParameter("@ADRequestor", ADRequestor));
-                        */
-                        //conn.Open();
-                        storedprocCommand.ExecuteNonQuery();
-                    }
-                    ///*_#########_END_NEW TRY_##########################################################################################################*/
-
-                }
-                /*_catch block_*/
-                catch (SqlException er)
-                {
-                    /*_Any SQL Server errors will be thrown here_*/
-                    Console.WriteLine("There was an error reported by SQL Server, " + er.Message);
-                }
-                /*_END_#######_ERROR HANDLING_*/
-
+                var refreshRequest = new RefreshRequest() {
+                    CompanyCode = CompanyCode,
+                    Destination = Destination,
+                    ADRequestor = ADRequestor,
+                    Database = database,
+                    EmailRequestor = EmailRequestor
+                };
+                refreshEnvironment(refreshRequest);
 
                 Console.WriteLine("___________________________________________________________________________________");
-                Console.WriteLine($"   Have a nice day! Thank you for using the APP Lower Environment Refresh Tool (ver 2)   ");
+                Console.WriteLine($"   Have a nice day! Thank you for using the APP Lower Environment Refresh Tool(ver 2)   ");
                 Console.WriteLine("___________________________________________________________________________________");
 
                 Console.ReadKey();
@@ -210,6 +134,84 @@ namespace ConsoleVer2_GitHub
 
             }
         }
+
+        private static void refreshEnvironment(RefreshRequest refreshRequest) {
+            Console.WriteLine("Submitting the parameters to the SP. Look for errors beyond this point!");
+
+            /*_try block_*/
+            try {
+                ///*_#########_BEGIN_ORIG TRY_##########################################################################################################*/
+                ///*_Create the command to execute!_*/
+                //                            /*_Test commandfail*/
+                //                            //SqlCommand commandfail = new SqlCommand("SELECT [FAIL] FROM [msst_test].[dbo].[Fail_vw_FAIL]", conn);
+                //SqlCommand storedprocCommand = new SqlCommand("Exec [UtilityDatabase].[dbo].[StoredProcedure] @Source = '" + Source + "', @Destination = '" + Destination + "', @Database = '" + Database + "', @Process = '" + Process + "', @CompanyCode = '" + CompanyCode + "', @EmailRequestor = '" + EmailRequestor + "',@ADRequestor = '" + ADRequestor + "'", conn);
+
+                ///*_Add the parameters._*/
+                //storedprocCommand.Parameters.Add(new SqlParameter("Source", Source));
+                //storedprocCommand.Parameters.Add(new SqlParameter("Destination", Destination));
+                //storedprocCommand.Parameters.Add(new SqlParameter("Database", Database));
+                //storedprocCommand.Parameters.Add(new SqlParameter("Process", Process));
+                //storedprocCommand.Parameters.Add(new SqlParameter("CompanyCode", CompanyCode));
+                //storedprocCommand.Parameters.Add(new SqlParameter("EmailRequestor", EmailRequestor));
+                //storedprocCommand.Parameters.Add(new SqlParameter("ADRequestor", ADRequestor));
+
+                ////Test commandfail will give on SQL error message
+                //// - Msg 208, Level 16, State 1, Line 4
+                ////Invalid object name 'someErrorColumn'.
+                ///*_ Errors will pop here:_*/
+                ///*_Note: since we are catching any code block errors, they will display here in the console.*/
+                //storedprocCommand.ExecuteNonQuery();
+
+                ///*Try to print the command being sent, make sure to remark out "storedprocCommand.ExecuteNonQuery();"*/
+                ////Console.WriteLine("___________________________________________________________________________________");
+                ////Console.WriteLine("Print the command: " + storedprocCommand + ""); //This writes "Print the command: System.Data.SqlClient.SqlCommand"
+                ////Console.WriteLine("___________________________________________________________________________________");
+                ///*_#########_END_ORIG TRY_##########################################################################################################*/
+
+                ///*_#########_BEGIN_NEW TRY_##########################################################################################################*
+                using(SqlCommand storedprocCommand = new SqlCommand("[dbo].[StoredProcedure]", conn)) {
+                    storedprocCommand.CommandType = CommandType.StoredProcedure;
+                    /*_NOTE:_*/
+                    //cmd.Parameters.Add("@FirstName", SqlDbType.VarChar).Value = txtFirstName.Text;
+                    //cmd.Parameters.Add("@LastName", SqlDbType.VarChar).Value = txtLastName.Text;
+                    /*My try
+                    storedprocCommand.Parameters.Add("@Source", SqlDbType.VarChar).Value = Source;
+                    storedprocCommand.Parameters.Add("@Destination", SqlDbType.VarChar).Value = Destination;
+                    storedprocCommand.Parameters.Add("@Database", SqlDbType.VarChar).Value = Database;
+                    storedprocCommand.Parameters.Add("@Process", SqlDbType.VarChar).Value = Process;
+                    storedprocCommand.Parameters.Add("@CompanyCode", SqlDbType.VarChar).Value = CompanyCode;
+                    storedprocCommand.Parameters.Add("@EmailRequestor", SqlDbType.VarChar).Value = EmailRequestor;
+                    storedprocCommand.Parameters.Add("@ADRequestor", SqlDbType.VarChar).Value = ADRequestor;
+                    */
+                    /*_Note:_Still getting time outs with this_
+                    storedprocCommand.Parameters.Add(new SqlParameter("@Source", Source));
+                    storedprocCommand.Parameters.Add(new SqlParameter("@Destination", Destination));
+                    storedprocCommand.Parameters.Add(new SqlParameter("@Database", Database));
+                    storedprocCommand.Parameters.Add(new SqlParameter("@Process", Process));
+                    storedprocCommand.Parameters.Add(new SqlParameter("@CompanyCode", CompanyCode));
+                    storedprocCommand.Parameters.Add(new SqlParameter("@EmailRequestor", EmailRequestor));
+                    storedprocCommand.Parameters.Add(new SqlParameter("@ADRequestor", ADRequestor));
+                    */
+                    //conn.Open();
+                    storedprocCommand.ExecuteNonQuery();
+                }
+                ///*_#########_END_NEW TRY_##########################################################################################################*/
+
+            }
+            /*_catch block_*/
+            catch(SqlException er) {
+                /*_Any SQL Server errors will be thrown here_*/
+                Console.WriteLine("There was an error reported by SQL Server, " + er.Message);
+            }
+            /*_END_#######_ERROR HANDLING_*/
+        }
+    }
+
+    public class RefreshRequest {
+        public string CompanyCode { get; set; }
+        string Destination { get; set; }
+        string Database { get; set; }
+        string ADRequestor { get; set; }
+        string EmailRequestor { get; set; }
     }
 }
-
